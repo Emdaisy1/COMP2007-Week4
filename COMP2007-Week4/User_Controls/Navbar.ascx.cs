e050b8901588619224ee87130cd43944280dc214
emdaisy1 <hilborn_emmav@live.ca>
2016-06-02 10:50:42 -0400

    Add project files.

---
+++ COMP2007-Week4/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/*
+ * @Author: Emma Hilborn - 200282755
+ */
+
+namespace COMP2007_Week4
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+
+
+        /**
+         * This method adds the "active" CSS class to the list items
+         * on the nav bar on each page
+         * 
+         * @method SetActivePage
+         * @return (void)
+         */
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
