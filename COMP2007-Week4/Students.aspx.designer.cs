3a3829eebfaf4ffedf3558517f240f25fe4a32d9
Emdaisy1 <hilborn_emmav@live.ca>
2016-06-08 16:34:31 -0400

    Student page gridview and DB connection

--- COMP2007-Week4/Students.aspx.designer.cs
+++ COMP2007-Week4/Students.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace COMP2007_Week4 {
     
     
     public partial class Students {
+        
+        /// <summary>
+        /// StudentsGridView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView StudentsGridView;
     }
 }
